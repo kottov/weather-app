0468dd01209aa16dfdd5445e5ae734b16e47681e
kottov <kottov@gmail.com>
2017-01-18 15:32:20 +0200

    32. Callback Errors

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
 /*jshint esversion: 6 */
 
-const req = require('request');
-
 var mapsApiUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
-var address = 'Yevhena Konovaltsia St, 15/2, Kyiv, Ukraine';
+
+const req = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        address: {
+            demand: true,
+            describe: 'Address to fetch weather for.',
+            alias: 'a',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
 
 req({
-    url: mapsApiUrl + address,
+    url: mapsApiUrl + encodeURIComponent(argv.address),
     json: true
 }, (err, res, body) => {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    if(err) {
+        console.log('Can not connect to server.');
+    } else if (body.status === 'ZERO_RESULTS') {
+        console.log('Unable to find that address.');
+    } else  if (body.status === 'OK') {
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    }
 });
